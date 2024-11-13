cf230e0855ac128b6d5416bef19f7cb5f7e27597
Noctwoa <edwardpaullynch@gmail.com>
2024-11-13 13:59:01 +0000

    13/11/2024
    Animation!!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb2d;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
     public float playSpeed;
 
     public Vector2 spawnPoint;
@@ -14,10 +16,15 @@ public class PlayerController : MonoBehaviour
     public int coins;
     public TextMeshProUGUI coinsText;
 
+    public bool isMoving;
+
+
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
         coinsText.text = "Coins: "+coins.ToString();
     }
 
@@ -26,6 +33,26 @@ public class PlayerController : MonoBehaviour
     {
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         float verticalInput = Input.GetAxisRaw("Vertical");
+
+        //flip the sprite (left or right)
+        if (horizontalInput < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (horizontalInput > 0)
+        {
+           spriteRenderer.flipX = false;
+        }
+
+        if (horizontalInput != 0||verticalInput != 0)
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
+        }
+        animator.SetBool("isMoving", isMoving);
 
         rb2d.velocity = new Vector2(horizontalInput * playSpeed, verticalInput * playSpeed);
     }
